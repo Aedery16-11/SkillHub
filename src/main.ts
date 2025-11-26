4cdf3d8b324c765b4b4c5e31d1ac394991b3b0f1
Aedery16-11 <aedery.16.11@gmail.com>
2025-11-26 15:50:38 +0100

    Fetch quiz questions from API and update question transformation logic

--- src/main.ts
+++ src/main.ts
@@ -3,81 +3,108 @@ import Alpine from "alpinejs";
 (window as any).Alpine = Alpine;
 
 type Question = {
-  id: string;
-  question: string;
-  answer: string;
-  categoryId: string;
-  category: string;
-  difficulty: string;
-  badAnswers: string[];
+    id: string;
+    question: string;
+    answer: string;
+    categoryId: string;
+    category: string;
+    difficulty: string;
+    badAnswers: string[];
 };
 
 const questions: Question[] = [
-  {
-    id: "cmck8wdut001shzrohzz0l96x",
-    question: "Quelle série de film de S-F a été réalisée par les Wachowski ?",
-    answer: "Matrix",
-    categoryId: "cmck8wdgi0003hzroikw9krfe",
-    category: "tv_cinema",
-    difficulty: "facile",
-    badAnswers: ["Star Wars", "Retour vers le futur", "Star Trek"],
-  },
-  {
-    id: "cmck8wdv7007khzroqkpjtogv",
-    question: "qui a réalisé le film Avatar ?",
-    answer: "James Cameron",
-    categoryId: "cmck8wdgi0003hzroikw9krfe",
-    category: "tv_cinema",
-    difficulty: "facile",
-    badAnswers: ["Bob Marley", "jean Dujardin", "Samuel Etienne"],
-  },
-  {
-    id: "cmguz1h970003hzaup5qeleap",
-    question: "Dans quelle série apparaît le personnage Walter White ?",
-    answer: "Breaking Bad",
-    categoryId: "cmck8wdgi0003hzroikw9krfe",
-    category: "tv_cinema",
-    difficulty: "facile",
-    badAnswers: ["Narcos", "Dexter", "The Sopranos"],
-  },
-  {
-    id: "cmck8wdus0013hzroulcqgayv",
-    question: "Qui animait le jeu télévisé le Bigdil ?",
-    answer: "Vincent Lagaf'",
-    categoryId: "cmck8wdgi0003hzroikw9krfe",
-    category: "tv_cinema",
-    difficulty: "facile",
-    badAnswers: ["Philippe Risoli", "Laurence Boccolini", "Nagui"],
-  },
-  {
-    id: "cmguz1h970005hzaufq8fqsrb",
-    question: "Quelle actrice incarne Katniss Everdeen dans 'Hunger Games' ?",
-    answer: "Jennifer Lawrence",
-    categoryId: "cmck8wdgi0003hzroikw9krfe",
-    category: "tv_cinema",
-    difficulty: "facile",
-    badAnswers: ["Emma Watson", "Scarlett Johansson", "Kristen Stewart"],
-  },
+    {
+        id: "cmck8wdut001shzrohzz0l96x",
+        question: "Quelle série de film de S-F a été réalisée par les Wachowski ?",
+        answer: "Matrix",
+        categoryId: "cmck8wdgi0003hzroikw9krfe",
+        category: "tv_cinema",
+        difficulty: "facile",
+        badAnswers: ["Star Wars", "Retour vers le futur", "Star Trek"],
+    },
+    {
+        id: "cmck8wdv7007khzroqkpjtogv",
+        question: "qui a réalisé le film Avatar ?",
+        answer: "James Cameron",
+        categoryId: "cmck8wdgi0003hzroikw9krfe",
+        category: "tv_cinema",
+        difficulty: "facile",
+        badAnswers: ["Bob Marley", "jean Dujardin", "Samuel Etienne"],
+    },
+    {
+        id: "cmguz1h970003hzaup5qeleap",
+        question: "Dans quelle série apparaît le personnage Walter White ?",
+        answer: "Breaking Bad",
+        categoryId: "cmck8wdgi0003hzroikw9krfe",
+        category: "tv_cinema",
+        difficulty: "facile",
+        badAnswers: ["Narcos", "Dexter", "The Sopranos"],
+    },
+    {
+        id: "cmck8wdus0013hzroulcqgayv",
+        question: "Qui animait le jeu télévisé le Bigdil ?",
+        answer: "Vincent Lagaf'",
+        categoryId: "cmck8wdgi0003hzroikw9krfe",
+        category: "tv_cinema",
+        difficulty: "facile",
+        badAnswers: ["Philippe Risoli", "Laurence Boccolini", "Nagui"],
+    },
+    {
+        id: "cmguz1h970005hzaufq8fqsrb",
+        question: "Quelle actrice incarne Katniss Everdeen dans 'Hunger Games' ?",
+        answer: "Jennifer Lawrence",
+        categoryId: "cmck8wdgi0003hzroikw9krfe",
+        category: "tv_cinema",
+        difficulty: "facile",
+        badAnswers: ["Emma Watson", "Scarlett Johansson", "Kristen Stewart"],
+    },
 ];
 
 type TransformedQuestion = Question & {
-  answers: {answer: string, isValid: boolean}[];
+    answers: {answer: string, isValid: boolean}[];
 };
 
 const transformedQuestions: TransformedQuestion[] = questions.map(
-  (question) => ({
-    ...question,
-    answers: [
-      question.answer,
-      ...question.badAnswers,
-    ].sort(() => Math.random() - 0.5),
-  })
+    (question) => ({
+        ...question,
+        answers: [
+            question.answer,
+            ...question.badAnswers,
+        ].sort(() => Math.random() - 0.5),
+    })
 );
 
+async function getData() {
+    const url = "https://quizzapi.jomoreschi.fr/api/v2/quiz?limit=5&category=tv_cinema&difficulty=facile";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result["quizzes"].map(
+            (question: Question) => ({
+                ...question,
+                answers: [
+                    question.answer,
+                    ...question.badAnswers,
+                ].sort(() => Math.random() - 0.5),
+            })
+        )
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
 Alpine.store("quizz", {
+    async init() {
+        this.questions = await getData()
+    },
     currentQuestionIndex: 0,
     selectedAnswer: '',
     isSubmitted: false,
+    questions: [],
     get currentQuestion() {
         return transformedQuestions[this.currentQuestionIndex]
     },
@@ -87,7 +114,7 @@ Alpine.store("quizz", {
         }
     },
     onNext() {
-        this.currentQuestionIndex++
+    this.currentQuestionIndex++
         this.isSubmitted = false
     },
     onPrevious(){
